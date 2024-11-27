d2266a7d05f8374568ac721f7baf967c9bba2899
Aimelyse Moss <amosekata@gmail.com>
2024-11-27 21:49:51 +0200

    Fix: Db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports:[
-        MongooseModule.forRoot('mongodb+srv://crmproject:evolve24CRM@devcrm.x6ua05t.mongodb.net/?retryWrites=true&w=majority&appName=devCrm')
-    ]
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://mfuraimelyse:tasYIvekKpocBmrc@heriwork.lgysl.mongodb.net/?retryWrites=true&w=majority&appName=heriwork',
+    ),
+  ],
 })
-export class DatabaseModule {
-
-}
+export class DatabaseModule {}
